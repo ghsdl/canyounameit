a11e3273dea22f5856e73eded98a7b4ac3ad880d
ghsdl <maeva.ghsdl@gmail.com>
2022-07-10 14:09:04 +0200

    searchbar component and styling

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Input } from 'react-rainbow-components';
+import { BiSearchAlt } from 'react-icons/bi';
+import styles from './SearchBar.scss';
+
+const SearchBar = ({ input, onChange, onSubmit }) => {
+  return (
+    <div className='search'>
+      <div className='rainbow-align-content_center rainbow-p-vertical_x-large rainbow-flex_wrap'>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmit();
+          }}
+        >
+          <Input
+            type='search'
+            label='application component search'
+            hideLabel
+            className='rainbow-p-around_medium search__input'
+            style={styles}
+            placeholder='Type your keywords'
+            isCentered
+            iconPosition='right'
+            icon={<BiSearchAlt className='rainbow-color_gray-3 search__icon' />}
+            value={input}
+            onChange={(event) => onChange(event.target.value)}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
