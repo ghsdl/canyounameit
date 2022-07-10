4d6f65f91009f1da599b5a3b1371fd8691011391
ghsdl <maeva.ghsdl@gmail.com>
2022-07-10 15:12:40 +0200

    header styling and transition when user starts searching

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import SearchBar from './components/SearchBar/SearchBar';
 
 const App = () => {
+  const [search, setSearch] = useState('');
+  const [headerMinimize, setHeaderMinimize] = useState(false);
+
   return (
     <div className='App'>
-      <Header />
-      <SearchBar />
+      <Header headerMinimize={headerMinimize} />
+      <SearchBar
+        input={search}
+        onChange={(search) => {
+          setSearch(search);
+          setHeaderMinimize(search.length);
+        }}
+        onSubmit={() => {}}
+      />
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import HEADER_LOGO from '../../assets/images/HEADER_LOGO.svg';
 import './Header.scss';
 
-const Header = () => {
+const Header = ({ headerMinimize }) => {
   return (
     <header className='header'>
-      <img className='header__logo' src={HEADER_LOGO} alt='logo' />
-      <h1 className='header__title'>Can you name it ?</h1>
+      <img
+        className={
+          headerMinimize
+            ? 'header__logo header__logo--small'
+            : ' header__logo header__logo--big'
+        }
+        src={HEADER_LOGO}
+        alt='logo'
+      />
+      <h1
+        className={
+          headerMinimize
+            ? 'header__title header__title--small'
+            : 'header__title header__title--big'
+        }
+      >
+        Can you name it ?
+      </h1>
     </header>
   );
 };
