c953ddfec156d810c529160a87844f419dc37141
ghsdl <maeva.ghsdl@gmail.com>
2022-07-11 21:43:17 +0200

    fix: effets de bord de react rainbow application

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Application, Button } from 'react-rainbow-components';
+import { Button } from 'react-rainbow-components';
 import './Results.scss';
 import Icon from '../Icon/Icon';
 import Result from '../Result/Result';
@@ -7,13 +7,7 @@ import Result from '../Result/Result';
 const Results = ({ madeUpNames }) => {
   const madeUpNamesPerRow = 6;
   const [next, setNext] = useState(madeUpNamesPerRow);
-  const theme = {
-    rainbow: {
-      palette: {
-        brand: '#f50057',
-      },
-    },
-  };
+
   return (
     <div className='results'>
       <div className='results__list'>
@@ -22,20 +16,15 @@ const Results = ({ madeUpNames }) => {
         ))}
       </div>
       {next < madeUpNames.length && (
-        <Application
-          theme={theme}
-          className='rainbow-p-vertical_large rainbow-align-content_center'
+        <Button
+          variant='brand'
+          className='results__button'
+          size='medium'
+          onClick={() => setNext(next + madeUpNamesPerRow)}
         >
-          <Button
-            variant='brand'
-            className='results__button'
-            size='medium'
-            onClick={() => setNext(next + madeUpNamesPerRow)}
-          >
-            Load more
-            <Icon icon='ARROW_DOWN' className='rainbow-m-left_medium' />
-          </Button>
-        </Application>
+          Load more
+          <Icon icon='ARROW_DOWN' className='results__icon' />
+        </Button>
       )}
     </div>
   );
