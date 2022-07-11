c674d3f6317764c9d2909be6c01f203304688052
ghsdl <maeva.ghsdl@gmail.com>
2022-07-11 17:54:51 +0200

    fix: width for results and useless form

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -7,25 +7,19 @@ const SearchBar = ({ input, onChange }) => {
   return (
     <div className='search'>
       <div className='rainbow-align-content_center rainbow-p-vertical_x-large rainbow-flex_wrap'>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-        >
-          <Input
-            type='search'
-            label='application component search'
-            hideLabel
-            className='rainbow-p-around_medium search__input'
-            style={styles}
-            placeholder='Type your keywords'
-            isCentered
-            iconPosition='right'
-            icon={<BiSearchAlt className='rainbow-color_gray-3 search__icon' />}
-            value={input}
-            onChange={(event) => onChange(event.target.value)}
-          />
-        </form>
+        <Input
+          type='search'
+          label='application component search'
+          hideLabel
+          className='rainbow-p-around_medium search__input'
+          style={styles}
+          placeholder='Type your keywords'
+          isCentered
+          iconPosition='right'
+          icon={<BiSearchAlt className='rainbow-color_gray-3 search__icon' />}
+          value={input}
+          onChange={(event) => onChange(event.target.value)}
+        />
       </div>
     </div>
   );
